6487abbec13497dcc19470f3be7cc0b18fb87896
Matt Dickson <35905014+mattedicksoncom@users.noreply.github.com>
2023-03-02 22:03:02 +0000

    prepping for the good stuff

--- funky_dancer.c
+++ funky_dancer.c
@@ -18,12 +18,37 @@ struct Vec3f {
 	float z;
 };
 
-
 void swap(int *xp, int *yp) {
 	int temp = *xp;
 	*xp = *yp;
 	*yp = temp;
 }
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Quaternion {
+	float w;
+	float x;
+	float y;
+	float z;
+};
+
+struct Transform {
+	struct Vector3 position;
+	struct Quaternion rotation;
+	struct Vector3 scale;
+};
+
+struct SceneObject {
+	struct Mesh *mesh;
+	struct Transform transform;
+	struct SceneObject *children[10]; // max 10 for now
+	int childCount;
+};
 
 void multiplyMatrixVector(float *m, float *v, float *out) {
 	out[0] = m[0]*v[0] + m[4]*v[1] + m[8]*v[2] + m[12]*v[3];
@@ -46,7 +71,7 @@ void trs(struct mesh *outMesh,
 
 	// Rotation matrix
 	float c, s, len;
-	float axis[3] = {rx, ry, rz};
+	float axis[3] = { rx, ry, rz };
 	len = sqrtf(axis[0]*axis[0] + axis[1]*axis[1] + axis[2]*axis[2]);
 	if (len != 0.0f) {
 		c = cosf(M_PI / 180.0f * len);
@@ -59,9 +84,9 @@ void trs(struct mesh *outMesh,
 		s = 0.0f;
 	}
 	float t2[16] = {
-		axis[0]*axis[0]*(1.0f-c)+c, axis[0]*axis[1]*(1.0f-c)-axis[2]*s, axis[0]*axis[2]*(1.0f-c)+axis[1]*s, 0.0f,
-		axis[0]*axis[1]*(1.0f-c)+axis[2]*s, axis[1]*axis[1]*(1.0f-c)+c, axis[1]*axis[2]*(1.0f-c)-axis[0]*s, 0.0f,
-		axis[0]*axis[2]*(1.0f-c)-axis[1]*s, axis[1]*axis[2]*(1.0f-c)+axis[0]*s, axis[2]*axis[2]*(1.0f-c)+c, 0.0f,
+		axis[0]*axis[0]*(1.0f-c) + c, axis[0]*axis[1]*(1.0f-c)-axis[2]*s, axis[0]*axis[2]*(1.0f-c) + axis[1]*s, 0.0f,
+		axis[0]*axis[1]*(1.0f-c) + axis[2]*s, axis[1]*axis[1]*(1.0f-c) + c, axis[1]*axis[2]*(1.0f-c)-axis[0]*s, 0.0f,
+		axis[0]*axis[2]*(1.0f-c)-axis[1]*s, axis[1]*axis[2]*(1.0f-c) + axis[0]*s, axis[2]*axis[2]*(1.0f-c) + c, 0.0f,
 		0.0f, 0.0f, 0.0f, 1.0f
 	};
 
@@ -76,11 +101,11 @@ void trs(struct mesh *outMesh,
 	// Transform each vertex
 	for (int i = 0; i < outMesh->vertCount; i++) {
 		// Apply translation, rotation, and scaling to vertex
-		float v[4] = {outMesh->vert[i*3], outMesh->vert[i*3 + 1], outMesh->vert[i*3 + 2], 1.0f};
+		float v[4] = { outMesh->vert[i*3], outMesh->vert[i*3 + 1], outMesh->vert[i*3 + 2], 1.0f };
 		float v2[4];
 		multiplyMatrixVector(t, v, v2);
 		multiplyMatrixVector(t2, v2, v);
-		multiplyMatrixVector(s3, v,v2);
+		multiplyMatrixVector(s3, v, v2);
 		// Store transformed vertex back into mesh
 		outMesh->vert[i*3] = v2[0];
 		outMesh->vert[i*3 + 1] = v2[1];
@@ -94,7 +119,7 @@ void generateSphere(float radius, int subdivisions, struct mesh *outMesh) {
 	int faceCount = 0;
 
 	// Allocate memory for verts and faces
-	outMesh->vert = (float*)malloc(sizeof(float) * 3 * (subdivisions+1) * (subdivisions+1));
+	outMesh->vert = (float*)malloc(sizeof(float) * 3 * (subdivisions + 1) * (subdivisions + 1));
 	outMesh->face = (int*)malloc(sizeof(int) * 6 * subdivisions * subdivisions);
 
 	// Generate verts
@@ -134,48 +159,48 @@ void generateSphere(float radius, int subdivisions, struct mesh *outMesh) {
 }
 
 // void line(int x0, int y0, int x1, int y1, SDL_Surface* surface, unsigned int color) { 
-void line(int x0, int y0, int x1, int y1, char* pixels, unsigned int color) { 
+void line(int x0, int y0, int x1, int y1, char* pixels, unsigned int color) {
 	// pitch: the length of a row of pixels in bytes (read-only)
 	// const int pitch = surface->pitch;
 	// char* pixels = surface->pixels;
 	// need a bounds check
 	int pitch = sizeof(char) * 2048;
 	
-	bool steep = false; 
-	if (abs(x0-x1) < abs(y0-y1)) { 
-		swap(&x0, &y0); 
-		swap(&x1, &y1); 
-		steep = true; 
-	} 
-	if (x0>x1) { 
-		swap(&x0, &x1); 
-		swap(&y0, &y1); 
-	} 
-	int dx = x1-x0; 
-	int dy = y1-y0; 
-	int derror2 = abs(dy)*2; 
-	int error2 = 0; 
-	int y = y0; 
-	for (int x = x0; x <= x1; x++) { 
-		if (steep) { 
-			 //image.set(y, x, color); 
+	bool steep = false;
+	if (abs(x0-x1) < abs(y0-y1)) {
+		swap(&x0, &y0);
+		swap(&x1, &y1);
+		steep = true;
+	}
+	if (x0 > x1) {
+		swap(&x0, &x1);
+		swap(&y0, &y1);
+	}
+	int dx = x1-x0;
+	int dy = y1-y0;
+	int derror2 = abs(dy)*2;
+	int error2 = 0;
+	int y = y0;
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			//image.set(y, x, color); 
 			if (x >= 0 && x < 512 && y >= 0 && y < 512) {
 				unsigned int* row = (unsigned int*)(pixels + pitch * y);
 				row[x] = color;
 			}
-		} else { 
-			 //image.set(x, y, color);
+		} else {
+			//image.set(x, y, color);
 			if (x >= 0 && x < 512 && y >= 0 && y < 512) {
 				unsigned int* row = (unsigned int*)(pixels + pitch * x);
 				row[y] = color;
 			}
-		} 
-		error2 += derror2; 
-		if (error2 > dx) { 
-			y += (y1>y0?1:-1); 
-			error2 -= dx*2; 
-		} 
-	} 
+		}
+		error2 += derror2;
+		if (error2 > dx) {
+			y += (y1 > y0?1:-1);
+			error2 -= dx*2;
+		}
+	}
 }
 
 //void draw_scene(SDL_Surface* surface, int width, int height, struct mesh *sphereMesh_ptr) {
@@ -218,7 +243,7 @@ void draw_scene(char* pixels, int width, int height, struct mesh *sphereMesh_ptr
 void cloneMesh(struct mesh *originalMesh, struct mesh *newMesh) {
 	// Allocate memory for the vertices and copy the values
 	//if (newMesh->vert != NULL) {
-		//free(newMesh->vert);
+	//free(newMesh->vert);
 	//}
 	newMesh->vert = malloc(sizeof(float) * originalMesh->vertCount * 3);
 	memcpy(newMesh->vert, originalMesh->vert, sizeof(float) * originalMesh->vertCount * 3);
@@ -226,7 +251,25 @@ void cloneMesh(struct mesh *originalMesh, struct mesh *newMesh) {
 
 	// Allocate memory for the faces and copy the values
 	//if (newMesh->face != NULL) {
-		//free(newMesh->face);
+	//free(newMesh->face);
+	//}
+	newMesh->face = malloc(sizeof(int) * originalMesh->faceCount * 3);
+	memcpy(newMesh->face, originalMesh->face, sizeof(int) * originalMesh->faceCount * 3);
+	newMesh->faceCount = originalMesh->faceCount;
+}
+
+void cloneMeshToScene(struct mesh *originalMesh, struct mesh *newMesh) {
+	// Allocate memory for the vertices and copy the values
+	//if (newMesh->vert != NULL) {
+	//free(newMesh->vert);
+	//}
+	newMesh->vert = malloc(sizeof(float) * originalMesh->vertCount * 3);
+	memcpy(newMesh->vert, originalMesh->vert, sizeof(float) * originalMesh->vertCount * 3);
+	newMesh->vertCount = originalMesh->vertCount;
+
+	// Allocate memory for the faces and copy the values
+	//if (newMesh->face != NULL) {
+	//free(newMesh->face);
 	//}
 	newMesh->face = malloc(sizeof(int) * originalMesh->faceCount * 3);
 	memcpy(newMesh->face, originalMesh->face, sizeof(int) * originalMesh->faceCount * 3);
@@ -234,13 +277,15 @@ void cloneMesh(struct mesh *originalMesh, struct mesh *newMesh) {
 }
 
 int main(int argc, char* argv[]) {
-    char *funkyString = "Starting the funk!";
-    printf("%s\n", funkyString);
+	char *funkyString = "Starting the funk!";
+	printf("%s\n", funkyString);
 
 	Uint32 startTicks, frameTicks;
 
 	SDL_Window* window = SDL_CreateWindow("Funky Dancer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512, 0);
 	// SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetWindowOpacity(window, 0.9);
+
 	SDL_CreateRenderer(window, -1, 0);
 
 	if (!window) {
@@ -249,7 +294,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	SDL_Surface* screen = SDL_GetWindowSurface(window);
-	SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormat(0, 512, 512,32, SDL_PIXELFORMAT_RGBX8888);
+	SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormat(0, 512, 512, 32, SDL_PIXELFORMAT_RGBX8888);
 
 	int finishTheFunk = 0;
 
@@ -257,6 +302,10 @@ int main(int argc, char* argv[]) {
 	struct mesh sphereMesh;
 	int subdivisions = 10;
 	generateSphere(1.0, subdivisions, &sphereMesh);
+	trs(&sphereMesh,
+	    0, 0, 0,
+	    0, 0, 0,
+	    0.5, 0.5, 1);
 
 	struct mesh sphereMeshShift;
 	generateSphere(0.2, 5, &sphereMeshShift);
@@ -267,7 +316,7 @@ int main(int argc, char* argv[]) {
 
 	struct mesh *allMeshes[500]; // limit to 500 for now
 	int meshCount = 0;
-	struct mesh *sceneMeshes[500]; // limit to 500 for now
+	struct mesh *sceneObjects[500]; // limit to 500 for now
 	int sceneMeshCount = 0;
 
 	allMeshes[meshCount++] = &sphereMesh;
@@ -281,10 +330,10 @@ int main(int argc, char* argv[]) {
 		struct mesh *newMesh = malloc(sizeof(struct mesh));
 
 		// Clone the original mesh into the new mesh
-		cloneMesh(originalMesh, newMesh);
+		cloneMeshToScene(originalMesh, newMesh);
 
 		// Add the new mesh to the scene meshes
-		sceneMeshes[i] = newMesh;
+		sceneObjects[i] = newMesh;
 
 		sceneMeshCount++;
 	}
@@ -308,26 +357,26 @@ int main(int argc, char* argv[]) {
 		for (int i = 0; i < meshCount; i++) {
 
 			// need to free them, see if can do a check in the function
-			free(sceneMeshes[i]->vert);
-			free(sceneMeshes[i]->face);
+			free(sceneObjects[i]->vert);
+			free(sceneObjects[i]->face);
 
 			struct mesh *originalMesh = allMeshes[i];
 
-			struct mesh *newMesh = sceneMeshes[i];
+			struct mesh *newMesh = sceneObjects[i];
 
 			// Clone the original mesh into the new mesh
-			cloneMesh(originalMesh, newMesh);
+			cloneMeshToScene(originalMesh, newMesh);
 
 			// Add the new mesh to the scene meshes
-			sceneMeshes[i] = newMesh;
+			sceneObjects[i] = newMesh;
 		}
 
-		trs(sceneMeshes[0],
+		trs(sceneObjects[0],
 		    0, 0, 0,
 		    0, 2 * delta, 1 * delta,
 		    1, 1, 1);
 
-		trs(sceneMeshes[1],
+		trs(sceneObjects[1],
 		    0, 0, 0,
 		    2 * delta, 5 * delta, 1.2 * delta,
 		    1, 1, 1);
@@ -337,7 +386,7 @@ int main(int argc, char* argv[]) {
 		char* pixels = surface->pixels;
 
 		for (int i = 0; i < sceneMeshCount; i++) {
-			draw_scene(pixels, 512, 512, sceneMeshes[i]);
+			draw_scene(pixels, 512, 512, sceneObjects[i]);
 		}
 
 		SDL_UnlockSurface(surface);
@@ -365,8 +414,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	for (int i = 0; i < meshCount; i++) {
-		free(sceneMeshes[i]->vert);
-		free(sceneMeshes[i]->face);
+		free(sceneObjects[i]->vert);
+		free(sceneObjects[i]->face);
 		printf("clear success\n");
 	}
 
