78d6d9c373ec32d978d301b15696e7a93238ad5e
Matt Dickson <35905014+mattedicksoncom@users.noreply.github.com>
2023-04-02 15:35:25 +0100

    add in resources to test loading external files

--- web_build.bat
+++ web_build.bat
@@ -1,3 +1,3 @@
 @echo off
-emcc -O3 -s USE_SDL=2 -s DETERMINISTIC=1 funky_dancer.c --shell-file em_template.html -o ./builds/web/funky_web.html
+emcc -O3 -s USE_SDL=2 -s DETERMINISTIC=1 funky_dancer.c --preload-file resources --shell-file em_template.html -o ./builds/web/funky_web.html
 echo Compilation complete.
